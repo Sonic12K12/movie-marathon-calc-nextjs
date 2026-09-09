042965cd006ae15bcbc2252f06edb323eb1762d0
Konstantin S. <k_schiwarov@yahoo.de>
2026-09-09 13:56:23 +0200

    add coming soon heading

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 export default function Home() {
   return (
-    <header className="max-w-3xl mx-auto mt-16">
-      <div className="flex flex-row justify-center">
-        <h1 className="text-6xl font-outfit font-bold text-text-main">Watch</h1>
-        <h1 className="text-6xl font-outfit font-bold text-accent">Time</h1>
-      </div>
-    </header>
+    <main className="max-w-3xl mx-auto mt-16">
+      <header>
+        <div className="flex flex-row justify-center">
+          <h1 className="text-6xl font-outfit font-bold text-text-main">
+            Watch
+          </h1>
+          <h1 className="text-6xl font-outfit font-bold text-accent">Time</h1>
+        </div>
+      </header>
+
+      <h2 className="text-6xl mx-auto mt-36">Coming Soon 🎬</h2>
+    </main>
   );
 }
